b41c9f7ce877c5f3ad858c26b711a95b00c22d33
David Cowden <dcow90@gmail.com>
2013-02-24 16:26:33 -0600

    make main view scrollable

--- src/edu/grinnell/sandb/Utility.java
+++ src/edu/grinnell/sandb/Utility.java
@@ -1,19 +1,8 @@
 package edu.grinnell.sandb;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
-
-import android.content.Context;
-import android.util.Log;
 
 public class Utility {
 	
