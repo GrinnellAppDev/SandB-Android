6d875cd4b6329c0f679c827ca311884177c4ed01
David Cowden <dcow90@gmail.com>
2013-03-01 17:42:45 -0600

    remove loading dialog because it somehow leaks; fix parser and add guid to article

--- src/edu/grinnell/sandb/xmlpull/WebRequestTask.java
+++ src/edu/grinnell/sandb/xmlpull/WebRequestTask.java
@@ -1,14 +1,11 @@
 package edu.grinnell.sandb.xmlpull;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -16,7 +13,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.util.Log;
-
 import edu.grinnell.sandb.xmlpull.WebRequestTask.Result;
 
 public class WebRequestTask extends AsyncTask<String, Void, Result> {
@@ -24,9 +20,7 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 	private Context mAppContext;
 	private RetrieveDataListener mRetrieveDataListener;
 	private ProgressDialog mStatus;
-	
-	private static final int 		MAX_ATTEMPTS 	= 3;
-	
+		
 	public static final String WRT 	= "WebRequestTask";
 	
 	public WebRequestTask(Context context, RetrieveDataListener rdl) {
@@ -38,7 +32,7 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 	/* Setup the progress bar. */
 	@Override
 	protected void onPreExecute() {
-		mStatus = ProgressDialog.show(mAppContext,"","Loading Feed...", true);
+		//mStatus = ProgressDialog.show(mAppContext,"","Loading Feed...", true);
 	}
 	
 	@Override
@@ -52,15 +46,12 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 		if (!networkEnabled(cm)) 
 			return (r.setCode(Result.NO_NETWORK));
 		
-		String menu = downloadDataFromServer(arg0[0]);
+		InputStream stream = downloadDataFromServer(arg0[0]);
 
-		if (menu == null)
+		if (stream == null)
 			return r.setCode(Result.NO_DATA);
-		else if (menu.equals(Integer.valueOf(Result.HTTP_ERROR).toString())) {
-			return r.setCode(Result.HTTP_ERROR);
-		}
 		
-		r.setValue(menu);
+		r.setStream(stream);
 		
 		return r.setCode(Result.SUCCESS);
 	}
@@ -70,17 +61,12 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 	@Override
 	protected void onPostExecute(Result result) {
 		
-		Log.i("getMenuTask", "menu loaded from the server");
+		Log.i(WRT, "xml loaded from server");
+		// dismiss dialogue..
+		//mStatus.dismiss();
 		
-		try {
-			// dismiss loading..
-			mStatus.dismiss();
-			// notify the UI thread listener ..
-			mRetrieveDataListener.onRetrieveData(result);
-		} catch (Exception e) {
-			Log.d("post execute", e.toString());
-		}
-		
+		// notify the UI thread listener ..
+		mRetrieveDataListener.onRetrieveData(result);
 		
 		super.onPostExecute(result);
 	}
@@ -92,33 +78,25 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 		return (n != null) && n.isConnectedOrConnecting();
 	}
 	
-	protected static String downloadDataFromServer(String urlstr) {
-		String r = null;
-		int attempts = 0;
-		try {				
-			while (attempts < MAX_ATTEMPTS) {
-				
-				//Log.i(HTTP, request);
-				HttpClient client = new DefaultHttpClient();
-				HttpPost post = new HttpPost(urlstr);
-				
-				HttpResponse response = client.execute(post);
-				//Log.i(HTTP, response.getStatusLine().toString());
-				// Make sure the result is okay.
-				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-					r = EntityUtils.toString(response.getEntity());
-					//Log.i(HTTP, "JSON = " + r);
-					break;
-				}
-				attempts++;
-			}
+	protected static InputStream downloadDataFromServer(String urlstr) {
+		InputStream stream = null;
+		try {
+			URL url = new URL(urlstr);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setReadTimeout(10000 /* milliseconds */);
+	        conn.setConnectTimeout(15000 /* milliseconds */);
+	        conn.setRequestMethod("GET");
+	        conn.setDoInput(true);
+	        // Starts the query
+	        conn.connect();
+	        stream = conn.getInputStream();
 		} catch (IOException e) {
 			Log.e(WRT, e.toString());
 			Log.e(WRT, e.getMessage());
 		} catch (ParseException p) {
 			Log.e("ParseException", p.toString());} 
 		
-		return (attempts == MAX_ATTEMPTS) ? Integer.valueOf(Result.HTTP_ERROR).toString() : r;
+		return stream;
 	}
 
 	
@@ -140,16 +118,16 @@ public class WebRequestTask extends AsyncTask<String, Void, Result> {
 		public static final int NO_DATA = 5;
 		
 		private int code;
-		private String value;
+		private InputStream stream;
 		
-		public Result (int resultCode, String resultValue) {
+		public Result (int resultCode, InputStream resultStream) {
 			code = resultCode;
-			value = resultValue;
+			stream = resultStream;
 		}
 		public Result () {this(-1, null);}
 		public int getCode() {return code;}
-		public String getValue() {return value;}
+		public InputStream getStream() {return stream;}
 		public Result setCode(int c) {code = c; return this;}
-		public Result setValue(String v) {value = v; return this;}
+		public Result setStream(InputStream s) {stream = s; return this;}
 	}
 }
--- src/edu/grinnell/sandb/xmlpull/XMLParseTask.java
+++ src/edu/grinnell/sandb/xmlpull/XMLParseTask.java
@@ -1,43 +1,62 @@
 package edu.grinnell.sandb.xmlpull;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.util.Xml;
 import edu.grinnell.sandb.xmlpull.XMLParseTask.Article;
 
-public class XMLParseTask extends AsyncTask<String, Void, List<Article>> {
+public class XMLParseTask extends AsyncTask<InputStream, Void, List<Article>> {
 
 	private Context mAppContext;
 	private ParseDataListener mParseDataListener;
 	private ProgressDialog mStatus;
+	
+	private static final String ns = null;
 		
 	public static final String XMP 	= "XMLParseTask";
 	
-	public XMLParseTask(Context context, ParseDataListener pdl) {
+	public XMLParseTask(Context appContext, ParseDataListener pdl) {
 		super();
-		mAppContext = context;
+		mAppContext = appContext;
 		mParseDataListener = pdl;		
 	}
 
-	/* Setup the progress bar. */
+	/* Setup the loading dialog. */
 	@Override
 	protected void onPreExecute() {
-		mStatus = ProgressDialog.show(mAppContext,"","Parsing Feed...", true);
+		//mStatus = ProgressDialog.show(mAppContext,"","Parsing Feed...", true);
 	}
 	
 	@Override
-	protected List<Article> doInBackground(String... arg0) {
-		
-		
-		List<Article> articles;
-		
-		articles = parseArticlesFromString(arg0[0]);
-		
-		return articles;
+	protected List<Article> doInBackground(InputStream... arg0) {
+				
+		try {
+			return parseArticlesFromStream(arg0[0]);
+		} catch (IOException ioe) {
+			Log.e(XMP, "parseArticlesFromStream", ioe);
+		} catch (XmlPullParserException xppe) {
+			Log.e(XMP, "parseArticlesFromStream", xppe);
+		} catch (Exception e) {
+			Log.e(XMP, "parseArticlesFromStream", e);
+		}
+		return new ArrayList<Article>();
 	}
 	
 	/* Stop the dialog and notify the main thread that the new menu
@@ -47,24 +66,93 @@ public class XMLParseTask extends AsyncTask<String, Void, List<Article>> {
 		
 		Log.i(XMP, "xml parsed!");
 		
-		try {
-			// dismiss loading..
-			mStatus.dismiss();
-			// notify the UI thread listener ..
-			mParseDataListener.onDataParsed(articles);
-		} catch (Exception e) {
-			Log.d(XMP, e.toString());
-		}
-		
+		// dismiss loading..
+		//mStatus.dismiss();
+		// notify the UI thread listener ..
+		mParseDataListener.onDataParsed(articles);
 		
 		super.onPostExecute(articles);
 	}
 	
-	protected static List<Article> parseArticlesFromString(String xmlstr) {
-		List<Article> articles = new ArrayList<Article>();
+	protected static List<Article> parseArticlesFromStream(InputStream xmlstream)
+		throws XmlPullParserException, IOException {
 		
-		return articles;
+		
+		Reader in = new InputStreamReader(xmlstream);
+		
+		 try {
+	            XmlPullParser parser = Xml.newPullParser();
+	            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+	            parser.setInput(in);
+	            parser.nextTag();
+	            return readFeed(parser);
+	        } finally {
+	            in.close();
+	        }	
 	}
+	
+	private static List<Article> readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List<Article> articles = new ArrayList<Article>();
+
+        parser.require(XmlPullParser.START_TAG, ns, "rss");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("channel")) {
+                continue;
+            } else if (name.equals("item")) {
+            	articles.add(readArticle(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return articles;
+    }
+	
+	// Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them
+    // off
+    // to their respective "read" methods for processing. Otherwise, skips the tag.
+    private static Article readArticle(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "item");
+        String title = null;
+        String guid = null;
+        String link = null;
+        String comments = null;
+        String date = null;
+        String category = null;
+        String description = null;
+        String body = null;
+        
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("guid")) {
+                link = readGuid(parser);
+            } else if (name.equals("link")) {
+                link = readLink(parser);
+            } else if (name.equals("comments")) {
+                comments = readComments(parser);
+            } else if (name.equals("date")) {
+                date = readDate(parser);
+            } else if (name.equals("category")) {
+                category = readCategory(parser);
+            } else if (name.equals("description")) {
+                description = readDescription(parser);
+            } else if (name.equals("content:encoded")) {
+                body = readBody(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Article(title, guid, link, comments, description, date, category, body);
+    }
 
 	
 	/* Listener you should implement for the callback method in the UI thread
@@ -73,18 +161,138 @@ public class XMLParseTask extends AsyncTask<String, Void, List<Article>> {
 		public void onDataParsed(List<Article> articles);
 	}
 	
-	public class Article {
-		private String title;
-		private String body;
+	public static class Article {
+		public String title;
+		public String id;
+		public String link;
+		public String comments;
+		public Date pubDate;
+		public String category;
+		public String description;
+		public String body;
 		
 		public Article (String articleTitle, String articleBody) {
 			title = articleTitle;
 			body  = articleBody;
 		}
-		public Article() {this("", "");}
-		public String getTitle() {return title;}
-		public String getBody() {return body;}
-		public Article setTitle(String t) {title = t; return this;}
-		public Article setBody(String b) {body = b; return this;}
+		
+		public Article (String articleTitle, String guid, String articleLink, String commentsLink,
+				String description, String publicationDate, String category, String articleBody) {
+			title = articleTitle;
+			id = guid;
+			link = articleLink;
+			comments = commentsLink;
+			this.category = category;
+			this.description = description;
+			body = articleBody;
+			try {
+				if (publicationDate != null) {
+					pubDate = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss ZZZZ", 
+							Locale.ENGLISH).parse(publicationDate);
+				} else {
+					pubDate = new Date();
+				}
+			} catch (ParseException pe) {
+				Log.d(XMP, "new Article", pe);
+			}
+		}
+		
+		public Article() {this("", "", "", "", "", "", "", "");}
+
 	}
+	
+    // Processes title tags in the feed.
+    private static String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Processes link tags in the feed.
+    private static String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "link");
+        String link = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "link");
+        return link;
+    }
+    
+ // Processes link tags in the feed.
+    private static String readGuid(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "guid");
+        String link = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "guid");
+        return link;
+    }
+
+    // Processes link tags in the feed.
+    private static String readComments(XmlPullParser parser) throws IOException, XmlPullParserException {
+    	parser.require(XmlPullParser.START_TAG, ns, "comments");
+        String commentsLink = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "comments");
+        return commentsLink;
+    }
+    
+    // Processes link tags in the feed.
+    private static String readDate(XmlPullParser parser) throws IOException, XmlPullParserException {
+    	parser.require(XmlPullParser.START_TAG, ns, "comments");
+        String date = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "comments");
+        return date;
+    }
+    
+    // Processes description tags in the feed.
+    private static String readDescription(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "description");
+        String description = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "description");
+        return description;
+    }
+    
+    // Processes description tags in the feed.
+    private static String readCategory(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "category");
+        String category = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "category");
+        return category;
+    }
+    
+    // Processes description tags in the feed.
+    private static String readBody(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "content:encoded");
+        String body = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "content:encoded");
+        return body;
+    }
+
+    // For the tags title and summary, extracts their text values.
+    private static String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+    
+    // Skips tags the parser isn't interested in. Uses depth to handle nested tags. i.e.,
+    // if the next tag after a START_TAG isn't a matching END_TAG, it keeps going until it
+    // finds the matching END_TAG (as indicated by the value of "depth" being 0).
+    private static void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+            case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+            case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+    
 }
