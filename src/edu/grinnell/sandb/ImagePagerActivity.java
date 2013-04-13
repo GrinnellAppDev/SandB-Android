50aea98befaad982141967cc50c955170c522ab4
Patrick Triest <triestpa@grinnell.edu>
2013-04-12 22:00:26 -0500

    Add image pager to show article images

---
+++ src/edu/grinnell/sandb/ImagePagerActivity.java
@@ -0,0 +1,162 @@
+package edu.grinnell.sandb;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+import edu.grinnell.grinnellsandb.R;
+
+public class ImagePagerActivity extends Activity {
+	
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+
+	//private static final String STATE_POSITION = "STATE_POSITION";
+
+	DisplayImageOptions options;
+
+	ViewPager pager;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_pager);
+
+		Bundle bundle = getIntent().getExtras();
+		String[] imageUrls = bundle.getStringArray("ArticleImages");
+		//int pagerPosition = bundle.getInt(Extra.IMAGE_POSITION, 0);
+
+		/*
+		if (savedInstanceState != null) {
+			pagerPosition = savedInstanceState.getInt(STATE_POSITION);
+		}
+		*/
+
+		options = new DisplayImageOptions.Builder()
+		//change these images to error messages
+			.showImageForEmptyUri(R.drawable.sandblogo)
+			.showImageOnFail(R.drawable.sandblogo)
+			.resetViewBeforeLoading()
+			.cacheOnDisc()
+			.imageScaleType(ImageScaleType.EXACTLY)
+			.bitmapConfig(Bitmap.Config.RGB_565)
+			.displayer(new FadeInBitmapDisplayer(300))
+			.build();
+
+		pager = (ViewPager) findViewById(R.id.pager);
+		pager.setAdapter(new ImagePagerAdapter(imageUrls));
+	//	pager.setCurrentItem(pagerPosition);
+	}
+/*
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		outState.putInt(STATE_POSITION, pager.getCurrentItem());
+	}
+*/
+	private class ImagePagerAdapter extends PagerAdapter {
+
+		private String[] images;
+		private LayoutInflater inflater;
+
+		ImagePagerAdapter(String[] images) {
+			this.images = images;
+			inflater = getLayoutInflater();
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			((ViewPager) container).removeView((View) object);
+		}
+
+		@Override
+		public void finishUpdate(View container) {
+		}
+
+		@Override
+		public int getCount() {
+			return images.length;
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup view, int position) {
+			View imageLayout = inflater.inflate(R.layout.item_pager_image, view, false);
+			ImageView imageView = (ImageView) imageLayout.findViewById(R.id.image);
+			final ProgressBar spinner = (ProgressBar) imageLayout.findViewById(R.id.loading);
+
+			ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(getApplicationContext());
+			imageLoader.init(configuration);
+			imageLoader.displayImage(images[position], imageView, options, new SimpleImageLoadingListener() {
+				@Override
+				public void onLoadingStarted(String imageUri, View view) {
+					spinner.setVisibility(View.VISIBLE);
+				}
+
+				@Override
+				public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+					String message = null;
+					switch (failReason.getType()) {
+						case IO_ERROR:
+							message = "Input/Output error";
+							break;
+						case DECODING_ERROR:
+							message = "Image can't be decoded";
+							break;
+						case NETWORK_DENIED:
+							message = "Downloads are denied";
+							break;
+						case OUT_OF_MEMORY:
+							message = "Out Of Memory error";
+							break;
+						case UNKNOWN:
+							message = "Unknown error";
+							break;
+					}
+					Toast.makeText(ImagePagerActivity.this, message, Toast.LENGTH_SHORT).show();
+
+					spinner.setVisibility(View.GONE);
+				}
+
+				@Override
+				public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+					spinner.setVisibility(View.GONE);
+				}
+			});
+
+			((ViewPager) view).addView(imageLayout, 0);
+			return imageLayout;
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return view.equals(object);
+		}
+
+		@Override
+		public void restoreState(Parcelable state, ClassLoader loader) {
+		}
+
+		@Override
+		public Parcelable saveState() {
+			return null;
+		}
+
+		@Override
+		public void startUpdate(View container) {
+		}
+	}
+}
