3777322b8739d4f3ec566350defa392eac5e94b7
David Cowden <dcow90@gmail.com>
2013-04-24 07:55:51 -0500

    remove unused import

--- src/edu/grinnell/sandb/MainActivity.java
+++ src/edu/grinnell/sandb/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
