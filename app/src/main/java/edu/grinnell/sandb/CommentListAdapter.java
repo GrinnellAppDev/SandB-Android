bcc0dd3c5e09dbf605dd43ad77f45b892c4efdf8
triestpa <triestpa@grinnell.edu>
2015-04-24 20:44:09 -0500

    Fix visual bugs from Android Studio refactoring

--- app/src/main/java/edu/grinnell/sandb/CommentListAdapter.java
+++ app/src/main/java/edu/grinnell/sandb/CommentListAdapter.java
@@ -1,13 +1,14 @@
 package edu.grinnell.sandb;
 
-import java.util.List;
-
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.List;
+
 import edu.grinnell.sandb.comments.Comment;
 
 /* List adapter to display the comments for a article */
