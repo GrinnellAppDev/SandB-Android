5bd9b88aece09ba779fa0e9d7476dbe63a448f32
Albert Owusu-Asare <owusuasa@grinnell.edu>
2016-03-16 01:02:22 -0500

    Clean up Constants

--- app/src/main/java/edu/grinnell/sandb/Constants.java
+++ app/src/main/java/edu/grinnell/sandb/Constants.java
@@ -7,11 +7,6 @@ public class Constants {
 
     // WordPress JSON API endpoint URL
     public static final String JSON_API_URL = "http://www.thesandb.com/api/get_recent_posts?count=50/";
-   // public static final String ENDPOINT_BASE_URL = "http://www.thesandb.com/api/";
-    public static final String ENDPOINT_BASE_URL = "http://www.thesandb.com/api/";
-    public static final String  PRABS_BASE_URL = "http://www.cs.grinnell.edu/";
-
-
 
     // convert the font size saved in settings to sp
     public static final int[] FONT_SIZE_TO_SP = {12, 14, 16, 18};
